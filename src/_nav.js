01ccba0430cf3315b4a15bef6ae0c7c1aee234b9
Tommy <hx.musavi@gmail.com>
2022-08-03 23:02:00 +0430

    tables

--- src/_nav.js
+++ src/_nav.js
@@ -1,34 +1,41 @@
-import React from 'react'
-import CIcon from '@coreui/icons-react'
-import { cilNotes } from '@coreui/icons'
-import { CNavGroup, CNavItem } from '@coreui/react'
+import React from "react";
+import CIcon from "@coreui/icons-react";
+import { cilNotes, cilPuzzle } from "@coreui/icons";
+import { CNavGroup, CNavItem } from "@coreui/react";
 
 const _nav = [
   {
     component: CNavItem,
-    name: 'Dashboard',
-    to: '/dashboard',
+    name: "Dashboard",
+    to: "/dashboard",
     badge: {
-      color: 'info',
+      color: "info",
     },
   },
   {
     component: CNavGroup,
-    name: 'Forms',
+    name: "Create Form",
     icon: <CIcon icon={cilNotes} customClassName="nav-icon" />,
     items: [
       {
         component: CNavItem,
-        name: 'Form1',
-        to: '/forms/validation',
+        name: "Form1",
+        to: "/forms/validation",
       },
       {
         component: CNavItem,
-        name: 'Form2',
-        to: '/forms/validation2',
+        name: "Form2",
+        to: "/forms/validation2",
       },
     ],
   },
-]
+  {
+    component: CNavGroup,
+    name: "Manage Orders",
+    to: "/base",
+    icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
+    items: [{ component: CNavItem, name: "Tables", to: "/base/tables" }],
+  },
+];
 
-export default _nav
+export default _nav;
--- src/views/forms/validation/Validation2.js
+++ src/views/forms/validation/Validation2.js
@@ -2,6 +2,28 @@ import { CCol, CForm, CFormInput } from "@coreui/react";
 import React from "react";
 
 export const Validation2 = () => {
+  const initialValues2 = {
+    buying_order_id:"",
+    number:"",
+    loading_date:"",
+    origin_weight:"",
+    departure_date:"",
+    first_weight:"",
+    second_weight:"",
+    sealed_weight:"",
+    pod:"",
+    bundle:"",
+    seller_net_weight:"",
+    buyer_net_weight:"",
+    container_number:"",
+    seal_number:"",
+    fixed_seller_lme_price:"",
+    fixed_buyer_lme_price:"",
+    fixed_buyer_lme_price_date:"",
+    lme_fixed_date:"",
+    lme_expiration_date:"",
+  };
+
   return (
     <CForm>
       <CCol xs={6}>
