30842c0b5f88114426894ca6dde8ef704ea94c69
Tommy <hx.musavi@gmail.com>
2022-09-02 15:15:24 +0430

    pagination handled in /agents

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { CPagination, CPaginationItem } from "@coreui/react";
+import { CPagination, CPaginationItem, CSpinner } from "@coreui/react";
 import { toast } from "react-toastify";
 
 // eslint-disable-next-line react/prop-types
-const Pagination = ({ url ,onUrlChange}) => {
+const Pagination = ({ url, onUrlChange }) => {
   const [link, setLink] = useState(url);
   const [meta, setMeta] = useState([]);
 
@@ -20,37 +20,56 @@ const Pagination = ({ url ,onUrlChange}) => {
     }
   };
 
-  // const prev = meta.shift();
-  // const next = meta.pop();
-
-  const lastIndex = meta.length - 1
-
+  const prev = meta[0].url;
+  const next = meta[lastIndex].url;
 
   useEffect(() => {
     getData();
   }, [link]);
+  const lastIndex = meta.length - 1;
 
- const makeItem = () => {
-   return (
-     <>
-     {meta.slice(1,lastIndex).map((item) => (
-       // eslint-disable-next-line react/jsx-key
-       <CPaginationItem active={item.active} key={item.url} onClick={() => {
-         setLink(item.url);onUrlChange(item.url)
-       }}>{item.label}</CPaginationItem>
-     ))}
-     </>
-   )
- }
+  const makeItem = () => {
+    return (
+      <>
+        {meta.slice(1, lastIndex).map((item) => (
+          // eslint-disable-next-line react/jsx-key
+          <CPaginationItem
+            active={item.active}
+            key={item.url}
+            onClick={() => {
+              setLink(item.url);
+              onUrlChange(item.url);
+            }}
+          >
+            {item.label}
+          </CPaginationItem>
+        ))}
+      </>
+    );
+  };
 
   return (
     <div>
       <CPagination aria-label="Page navigation example">
-        <CPaginationItem aria-label="Previous" disabled>
+        <CPaginationItem
+          aria-label="Previous"
+          onClick={() => {
+            setLink(meta[0].url);
+            onUrlChange(meta[0].url);
+          }}
+          disabled={prev ? false : true}
+        >
           <span aria-hidden="true">&laquo;</span>
         </CPaginationItem>
         {makeItem()}
-        <CPaginationItem aria-label="Next" disabled>
+        <CPaginationItem
+          aria-label="Next"
+          onClick={() => {
+            setLink(meta[lastIndex].url);
+            onUrlChange(meta[lastIndex].url);
+          }}
+          disabled={next ? false : true}
+        >
           <span aria-hidden="true">&raquo;</span>
         </CPaginationItem>
       </CPagination>
